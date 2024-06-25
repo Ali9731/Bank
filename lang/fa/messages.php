4a23665c95a8e77089e863df272f77bd9207b6b6
Ali9731 <alisadeghi.work@gmail.com>
2024-06-26 01:23:05 +0330

    lang added

---
+++ lang/fa/messages.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'card_not_found' => 'شماره کارت وارد شده در سیستم موجود نمیباشد : :CARD',
+    'impossible_transaction' => 'تراکنش امکان پذیر نمیباشد',
+    'insufficient_balance' => 'موجودی حساب برای انجام تراکنش کافی نیست.',
+    'transaction_failed' => 'عملیات با شکست مواجه شد.',
+    'transaction_successful' => 'عملیات با موفقیت انجام شد. کد پیگیری : :CODE',
+    'from_card_field.required' => 'فیلد کارت مبدا الزامی میباشد',
+    'from_card_field.invalid' => 'مقدار کارت مبدا اشتباه است',
+    'from_card_field.regex' => 'شماره کارت مبدا نا معتبر است',
+    'to_card_field.required' => 'فیلد کارت مقصد الزامی میباشد',
+    'to_card_field.invalid' => 'مقدار کارت مقصد اشتباه است',
+    'to_card_field.regex' => 'شماره مقصد کارت نا معتبر است',
+    'amount_field.required' => 'فیلد مبلغ الزامی میباشد',
+    'sms.successful_transaction' => 'تراکنش موفق
+    مبلغ تراکنش : :AMOUNT
+    کد پیگیری : :CODE',
+    'sms.transaction_income' => 'مبلغ :AMOUNT
+    از شماره کارت :CARD
+    به حساب شما واریز شد',
+
+];
